c0482cb56fb3274696c334d7bf7e03a246f60680
Mathana Nair Sreedaran <mathana96@gmail.com>
2018-10-24 01:33:34 +0100

    Add actionbar buttons - add and cancel
    Signed-off-by: Mathana Nair Sreedaran <mathana96@gmail.com>

--- app/src/main/java/org/mathana/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/mathana/hillfort/activities/HillfortActivity.kt
@@ -2,6 +2,8 @@ package org.mathana.hillfort.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -20,22 +22,39 @@ class HillfortActivity : AppCompatActivity(), AnkoLogger {
     setContentView(R.layout.activity_hillfort)
     app = application as MainApp
 
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
+
     btnAdd.setOnClickListener {
       hillfort.title = hillfortTitle.text.toString()
       hillfort.description = hillfortDescription.text.toString()
 
       if (hillfort.title.isNotEmpty() && hillfort.description.isNotEmpty()) {
-
         app.hillforts.add(hillfort.copy())
         info("Add Button Pressed: $hillfort")
         app.hillforts.forEach{ info("add button pressed: ${it.title} ${it.description}")}
+        setResult(AppCompatActivity.RESULT_OK)
+        finish()
       }
       else {
-
         val toastTitleDesc: String = getString(R.string.toast_enterTitleandDesc)
         toast (toastTitleDesc)
       }
     }
 
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_hillfort, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> {
+        finish()
+      }
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
--- app/src/main/java/org/mathana/hillfort/activities/HillfortListActivity.kt
+++ app/src/main/java/org/mathana/hillfort/activities/HillfortListActivity.kt
@@ -3,7 +3,10 @@ package org.mathana.hillfort.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import org.jetbrains.anko.startActivityForResult
 import org.mathana.hillfort.R
 import org.mathana.hillfort.adapters.HillfortAdapter
 import org.mathana.hillfort.main.MainApp
@@ -20,5 +23,20 @@ class HillfortListActivity: AppCompatActivity() {
     val layoutManager = LinearLayoutManager(this)
     recyclerView.layoutManager = layoutManager
     recyclerView.adapter = HillfortAdapter(app.hillforts)
+
+    toolbarMain.title = title
+    setSupportActionBar(toolbarMain)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_add -> startActivityForResult<HillfortActivity>(0)
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
