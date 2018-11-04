3f2fceb206e5a363a0d87314a9b983c686c60d67
Mathana Nair Sreedaran <mathana96@gmail.com>
2018-11-04 00:23:34 +0000

    Improve settings with toolbar
    Toolbar has cancel, up and logout
    
    Signed-off-by: Mathana Nair Sreedaran <mathana96@gmail.com>

--- app/src/main/java/org/mathana/hillfort/activities/SettingsActivity.kt
+++ app/src/main/java/org/mathana/hillfort/activities/SettingsActivity.kt
@@ -2,6 +2,9 @@ package org.mathana.hillfort.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.jetbrains.anko.AnkoLogger
@@ -9,8 +12,6 @@ import org.jetbrains.anko.info
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 import org.mathana.hillfort.R
-import org.mathana.hillfort.R.id.btn_logout
-import org.mathana.hillfort.R.id.btn_save_settings
 
 import org.mathana.hillfort.main.MainApp
 
@@ -25,13 +26,8 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
 
     app = application as MainApp
 
-
-    btn_logout.setOnClickListener {
-      info ("Logout button clicked")
-      finishAffinity(); //https://stackoverflow.com/a/9580057/8083587
-      startActivity<LoginActivity>()
-      finish()
-    }
+    setSupportActionBar(toolbarSettings)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     btn_save_settings.setOnClickListener {
       info ("Saved settings clicked")
@@ -39,5 +35,28 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
 
   }
 
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_settings, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> {
+        finish()
+      }
+      R.id.item_logout -> {
+        info ("Logout button clicked")
+        finishAffinity(); //https://stackoverflow.com/a/9580057/8083587
+        startActivity<LoginActivity>()
+        finish()
+      }
+      android.R.id.home -> {
+        finish()
+        return true
+      }
+    }
+    return super.onOptionsItemSelected(item)
+  }
 
 }
