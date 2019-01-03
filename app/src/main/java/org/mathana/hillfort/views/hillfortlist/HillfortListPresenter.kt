4a30fecf7de4dca5c1b2936e0c13ceb13b87a59b
Mathana Nair Sreedaran <mathana96@gmail.com>
2019-01-03 19:43:58 +0000

    Fav filter working
    Signed-off-by: Mathana Nair Sreedaran <mathana96@gmail.com>

--- app/src/main/java/org/mathana/hillfort/views/hillfortlist/HillfortListPresenter.kt
+++ app/src/main/java/org/mathana/hillfort/views/hillfortlist/HillfortListPresenter.kt
@@ -1,5 +1,8 @@
 package org.mathana.hillfort.views.hillfortlist
 
+import android.widget.CompoundButton
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.startActivity
 import org.mathana.hillfort.activities.HillfortsMapView
@@ -12,7 +15,7 @@ import org.mathana.hillfort.views.BaseView
 import org.mathana.hillfort.views.VIEW
 import org.mathana.hillfort.views.hillfort.HillfortView
 
-class HillfortListPresenter(view: BaseView): BasePresenter(view) {
+class HillfortListPresenter(view: BaseView): BasePresenter(view), AnkoLogger {
 
   var current_user = UserModel()
 
@@ -26,16 +29,22 @@ class HillfortListPresenter(view: BaseView): BasePresenter(view) {
 
   fun loadHillforts() = view?.showHillforts(app.users.findAllHillforts(current_user))
 
-//  fun doAddHillfort() { view.startActivityForResult(view.intentFor<HillfortView>().putExtra("current_user", current_user), 0) }
   fun doAddHillfort() { view?.navigateTo(VIEW.HILLFORT, 0, "current_user", current_user) }
 
-//  fun doShowHillfortMap() { view.startActivity<HillfortsMapView>() }
   fun doShowHillfortMap() { view?.navigateTo(VIEW.MAPS) }
 
-//  fun doSettings() { view.startActivityForResult(view.intentFor<SettingsActivity>().putExtra("current_user", current_user), 0) }
   fun doSettings() { view?.navigateTo(VIEW.SETTINGS, 0, "current_user", current_user) }
 
-//  fun doEditHillfort(hillfort: HillfortModel) { view. startActivityForResult(view.intentFor<HillfortView>().putExtra("hillfort_edit", hillfort).putExtra("current_user", current_user), 0)}
   fun doEditHillfort(hillfort: HillfortModel) { view?.navigateToHillfort(VIEW.HILLFORT, 0, "current_user", current_user, "hillfort_edit", hillfort) }
+
+  fun doFavSwitch(buttonView: CompoundButton, isChecked: Boolean) {
+    if (isChecked) {
+      info("fav switch selected on")
+      view?.showHillforts(app.users.findAllHillforts(current_user).filter { h -> h.fav == true })
+    } else {
+      info("fav switch selected off")
+      loadHillforts()
+    }
+  }
 
 }
