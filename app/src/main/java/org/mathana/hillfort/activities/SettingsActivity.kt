172ae0b1aeb2b7f3b381f749fce4678915e79238
Mathana Nair Sreedaran <mathana96@gmail.com>
2019-01-04 01:20:31 +0000

    Able to change email
    Signed-off-by: Mathana Nair Sreedaran <mathana96@gmail.com>

--- app/src/main/java/org/mathana/hillfort/activities/SettingsActivity.kt
+++ app/src/main/java/org/mathana/hillfort/activities/SettingsActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_settings.*
 import org.jetbrains.anko.*
 import org.mathana.hillfort.R
@@ -28,29 +30,29 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
     setSupportActionBar(toolbarSettings)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-
     settings_username.setText(user!!.email)
-//      settings_password.setText(current_user.password)
-//      total_hillforts.setText(getTotalHillforts(current_user))
-//      hillforts_visited.setText(getHillfortsVisited(current_user))
-//      info("This is the logged in user: $current_user")
 
 
-//    btn_save_settings.setOnClickListener {
-//      info ("Saved settings clicked")
-//      current_user.username = settings_username.text.toString()
-//      current_user.password = settings_password.text.toString()
-//      app.users.updateUser(current_user.copy())
-//      toast("Details updated")
-//      startActivityForResult(intentFor<HillfortListView>().putExtra("current_user", current_user), 0)
-//      finish()
-//    }
-//
+
+    btn_save_settings.setOnClickListener {
+      info ("Saved settings clicked")
+
+      user!!.updateEmail(settings_username.text.toString()).addOnCompleteListener { task ->
+        if (task.isSuccessful) {
+          toast("Details updated")
+        } else {
+          println("Error Update")
+          toast("Error Update")
+        }
+      }
+      startActivity(intentFor<HillfortListView>())
+      finish()
+    }
+
 //    btn_delete_account.setOnClickListener {
 //      info ("Delete button clicked")
 //      app.users.deleteUser(current_user.copy())
-//      finishAffinity();
+//      finishAffinity()
 //      startActivity<LoginView>()
 //      finish()
 //    }
@@ -76,20 +78,5 @@ class SettingsActivity: AppCompatActivity(), AnkoLogger {
     return super.onOptionsItemSelected(item)
   }
 
-//  fun getTotalHillforts(user: UserModel) : String {
-//    val foundUser: UserModel? = app.users.findAllUsers().find { u -> u.id == user.id }
-//    return foundUser!!.hillforts.size.toString()
-//  }
-//
-//  fun getHillfortsVisited(user: UserModel) : String {
-//    val foundUser: UserModel? = app.users.findAllUsers().find { u -> u.id == user.id }
-//    var count = 0
-//    for (hillfort in foundUser!!.hillforts) {
-//      if (hillfort.explored == true)
-//        count ++
-//    }
-//    info("COUNT: $count")
-//    return count.toString()
-//  }
 
 }
